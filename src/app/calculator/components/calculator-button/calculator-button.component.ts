c86409dc23fda2f9aea52d4186dd93be785e65d6
Luciano Figueroa <lumusitech@gmail.com>
2024-09-03 16:41:16 -0300

    refactor: extract calculator button into component

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -0,0 +1,46 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  input,
+} from '@angular/core';
+
+@Component({
+  selector: 'calculator-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator-button.component.html',
+  styleUrls: ['./calculator-button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'w-1/4 border-r border-b border-indigo-400',
+    //? We can add more attributes to the host component
+    // attribute: 'something',
+    // 'x-data': 'XL',
+  },
+})
+export class CalculatorButtonComponent {
+  // property
+  public isCommand = input(false, {
+    //? transform method is called before the signal value is set, before ngOnInit
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  // property
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  //? is-command is a local CSS class, so if we use it here,
+  //? it will not be applied to the host component,
+  // @HostBinding('class.is-command') get commandStyle() {
+  //   return this.isCommand();
+  // }
+
+  //? w-2/4 is a GLOBAL tailwind class, so we can use it in any component
+  @HostBinding('class.w-2/4') get doubleSizeStyle() {
+    return this.isDoubleSize();
+  }
+}
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 @Component({
   selector: 'calculator',
   standalone: true,
-  imports: [],
+  imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  // styles: `
+  //   .is-command {
+  //     @apply bg-indigo-700 bg-opacity-20;
+  //   }
+  // `,
 })
 export class CalculatorComponent {}
