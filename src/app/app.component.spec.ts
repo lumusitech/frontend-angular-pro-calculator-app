978692824b9c141a4de58c9320b7a45f540dd77f
Luciano Figueroa <lumusitech@gmail.com>
2024-09-19 13:53:04 -0300

    test: extract repeated code at top level variable to reuse

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, type ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    // this is a wrapper for the component
+    //it gives us access to the component instance and other utils functions
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    // literally give us access to the component instance
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  // it(`should have the 'frontend-angular-zoneless-calculator-app' title`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('frontend-angular-zoneless-calculator-app');
-  // });
+  // my first basic test
+  it('should be 2', () => {
+    // Arrange
+    const a = 1;
+    const b = 1;
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-angular-zoneless-calculator-app');
-  // });
+    // Act
+    const result = a + b;
+
+    // Assert
+    expect(result).toBe(2);
+  });
+
+  it(`should have the 'frontend-angular-zoneless-calculator-app' title`, () => {
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('frontend-angular-zoneless-calculator-app');
+  });
+
+  it('should has a router-outlet', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    console.log(compiled);
+
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
 });
